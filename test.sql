2d08b769c7cf87de45df8ab3065d015c59a66958
Azim <66790056+Azim-js@users.noreply.github.com>
2021-04-04 11:49:21 +0530

    -- tables along with queries

--- test.sql
+++ test.sql
@@ -1,116 +1,133 @@
--- CREATE TABLE employee(
---     emp_id INT PRIMARY KEY,
---     first_name VARCHAR(20),
---     last_name VARCHAR(20),
---     birth_day DATE,
---     sex VARCHAR(1),
---     salary INT,
---     super_id INT,
---     branch_id INT
--- );
+-- database company
+CREATE DATABASE company;
 
--- CREATE TABLE branch(
---     branch_id INT PRIMARY KEY,
---     branch_name VARCHAR(20),
---     mgr_id INT,
---     mgr_start_date,
---     FOREIGN KEY(mgr_id) REFERENCES epmloyee(empi_id) ON DELETE SET NULL
--- );
+-- TAbles
+-- employee table
+CREATE TABLE employee(
+    emp_id INT PRIMARY KEY,
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
+    birth_day DATE,
+    sex VARCHAR(1),
+    salary INT,
+    super_id INT,
+    branch_id INT
+);
 
--- ALTER TABLE employee
--- ADD FOREIGN KEY(branch_id) REFERENCES branch(branch_id) ON DELETE SET NULL;
+-- branch table
 
--- ALTER TABLE employee
--- ADD FOREIGN KEY(super_id) REFERENCES employee(empi_id) ON DELETE SET NULL;
+CREATE TABLE branch(
+    branch_id INT PRIMARY KEY,
+    branch_name VARCHAR(20),
+    mgr_id INT,
+    mgr_start_date,
+    FOREIGN KEY(mgr_id) REFERENCES epmloyee(empi_id) ON DELETE SET NULL
+);
 
--- CREATE TABLE client(
---     client_id INT PRIMARY KEY,
---     client_name VARCHAR(20),
---     branch_id INT,
---     FOREIGN KEY(branch_id) REFERENCES branch(branch_id) ON DELETE SET NULL
--- );
+-- relation of employee table and branch table by foreign key 
 
--- CREATE TABLE works_with(
---     emp_id INT,
---     client_id INT,
---     total_sales INT,
---     PRIMARY KEY(emp_id,client_id),
---     FOREIGN KEY(emp_id) REFERENCES employee(emp_id) ON DELETE CASCADE,
---     FOREIGN KEY(client_id) REFERENCES client(client_id) ON DELETE CASCADE
--- );
+ALTER TABLE employee
+ADD FOREIGN KEY(branch_id) REFERENCES branch(branch_id) ON DELETE SET NULL;
 
--- CREATE TABLE branch_supplier(
---     branch_id INT,
---     supplier_name VARCHAR(40),
---     supply_type VARCHAR(40),
---     PRIMARY KEY(branch_id,supplier_name),
---     FOREIGN KEY(branch_id) REFERENCES branch(branch_id) ON DELETE CASCADE
--- );
+ALTER TABLE employee
+ADD FOREIGN KEY(super_id) REFERENCES employee(empi_id) ON DELETE SET NULL;
 
--- INSERT INTO employee VALUES(100,'David','wallece','1967-11-17','M',25000,NULL,Null);
+-- client table
 
--- INSERT INTO branch VALUES(1,'Corporate',100,'2006-02-09');
+CREATE TABLE client(
+    client_id INT PRIMARY KEY,
+    client_name VARCHAR(20),
+    branch_id INT,
+    FOREIGN KEY(branch_id) REFERENCES branch(branch_id) ON DELETE SET NULL
+);
 
--- UPDATE employee 
--- SET branch_id=1
--- WHERE emp_id=100;
+-- works_with table the relationship table
 
--- INSERT INTO employee VALUES(101,'Jan','Levison','1961-05-11','F',11000,100,1);
+CREATE TABLE works_with(
+    emp_id INT,
+    client_id INT,
+    total_sales INT,
+    PRIMARY KEY(emp_id,client_id),
+    FOREIGN KEY(emp_id) REFERENCES employee(emp_id) ON DELETE CASCADE,
+    FOREIGN KEY(client_id) REFERENCES client(client_id) ON DELETE CASCADE
+);
+
+-- branch_ supplier table
+
+CREATE TABLE branch_supplier(
+    branch_id INT,
+    supplier_name VARCHAR(40),
+    supply_type VARCHAR(40),
+    PRIMARY KEY(branch_id,supplier_name),
+    FOREIGN KEY(branch_id) REFERENCES branch(branch_id) ON DELETE CASCADE
+);
+
+-- insertion of values to the tables 
+
+INSERT INTO employee VALUES(100,'David','wallece','1967-11-17','M',25000,NULL,Null);
+
+INSERT INTO branch VALUES(1,'Corporate',100,'2006-02-09');
+
+UPDATE employee 
+SET branch_id=1
+WHERE emp_id=100;
+
+INSERT INTO employee VALUES(101,'Jan','Levison','1961-05-11','F',11000,100,1);
 
 -- Scranton
 
--- INSERT INTO employee VALUES(102,'Michael','Scott','1964-03-15','M',75000,100,NULL);
--- INSERT INTO branch VALUES(2,'Scranton',102,'1992-04-06');
+INSERT INTO employee VALUES(102,'Michael','Scott','1964-03-15','M',75000,100,NULL);
+INSERT INTO branch VALUES(2,'Scranton',102,'1992-04-06');
 
--- UPDATE employee
--- SET branch_id=2
--- WHERE emp_id=102;
+UPDATE employee
+SET branch_id=2
+WHERE emp_id=102;
 
--- INSERT INTO employee VALUES(103, 'Angela', 'Martin', '1971-06-25', 'F', 63000, 102, 2);
--- INSERT INTO employee VALUES(104, 'Kelly', 'Kapoor', '1980-02-05', 'F', 55000, 102, 2);
--- INSERT INTO employee VALUES(105, 'Stanley', 'Hudson', '1958-02-19', 'M', 69000, 102, 2);
+INSERT INTO employee VALUES(103, 'Angela', 'Martin', '1971-06-25', 'F', 63000, 102, 2);
+INSERT INTO employee VALUES(104, 'Kelly', 'Kapoor', '1980-02-05', 'F', 55000, 102, 2);
+INSERT INTO employee VALUES(105, 'Stanley', 'Hudson', '1958-02-19', 'M', 69000, 102, 2);
 
 -- Stamford
 
--- INSERT INTO employee VALUES(106,'Josh','Potter','1969-09-05','M',75000,100,NULL);
--- INSERT INTO branch VALUES(3,'Stamford',106,'1998-02-13');
+INSERT INTO employee VALUES(106,'Josh','Potter','1969-09-05','M',75000,100,NULL);
+INSERT INTO branch VALUES(3,'Stamford',106,'1998-02-13');
 
--- UPDATE employee
--- SET branch_id=3
--- WHERE emp_id=106;
+UPDATE employee
+SET branch_id=3
+WHERE emp_id=106;
 
--- INSERT INTO employee VALUES(107, 'Andy', 'Bernard', '1973-07-22', 'M', 65000, 106, 3);
--- INSERT INTO employee VALUES(108, 'Jim', 'Halpert', '1978-10-01', 'M', 71000, 106, 3);
+INSERT INTO employee VALUES(107, 'Andy', 'Bernard', '1973-07-22', 'M', 65000, 106, 3);
+INSERT INTO employee VALUES(108, 'Jim', 'Halpert', '1978-10-01', 'M', 71000, 106, 3);
 
 
 -- -- BRANCH SUPPLIER
--- INSERT INTO branch_supplier VALUES(2, 'Hammer Mill', 'Paper');
--- INSERT INTO branch_supplier VALUES(2, 'Uni-ball', 'Writing Utensils');
--- INSERT INTO branch_supplier VALUES(3, 'Patriot Paper', 'Paper');
--- INSERT INTO branch_supplier VALUES(2, 'J.T. Forms & Labels', 'Custom Forms');
--- INSERT INTO branch_supplier VALUES(3, 'Uni-ball', 'Writing Utensils');
--- INSERT INTO branch_supplier VALUES(3, 'Hammer Mill', 'Paper');
--- INSERT INTO branch_supplier VALUES(3, 'Stamford Lables', 'Custom Forms');
+INSERT INTO branch_supplier VALUES(2, 'Hammer Mill', 'Paper');
+INSERT INTO branch_supplier VALUES(2, 'Uni-ball', 'Writing Utensils');
+INSERT INTO branch_supplier VALUES(3, 'Patriot Paper', 'Paper');
+INSERT INTO branch_supplier VALUES(2, 'J.T. Forms & Labels', 'Custom Forms');
+INSERT INTO branch_supplier VALUES(3, 'Uni-ball', 'Writing Utensils');
+INSERT INTO branch_supplier VALUES(3, 'Hammer Mill', 'Paper');
+INSERT INTO branch_supplier VALUES(3, 'Stamford Lables', 'Custom Forms');
 
 -- -- CLIENT
--- INSERT INTO client VALUES(400, 'Dunmore Highschool', 2);
--- INSERT INTO client VALUES(401, 'Lackawana Country', 2);
--- INSERT INTO client VALUES(402, 'FedEx', 3);
--- INSERT INTO client VALUES(403, 'John Daly Law, LLC', 3);
--- INSERT INTO client VALUES(404, 'Scranton Whitepages', 2);
--- INSERT INTO client VALUES(405, 'Times Newspaper', 3);
--- INSERT INTO client VALUES(406, 'FedEx', 2);
+INSERT INTO client VALUES(400, 'Dunmore Highschool', 2);
+INSERT INTO client VALUES(401, 'Lackawana Country', 2);
+INSERT INTO client VALUES(402, 'FedEx', 3);
+INSERT INTO client VALUES(403, 'John Daly Law, LLC', 3);
+INSERT INTO client VALUES(404, 'Scranton Whitepages', 2);
+INSERT INTO client VALUES(405, 'Times Newspaper', 3);
+INSERT INTO client VALUES(406, 'FedEx', 2);
 
 -- -- WORKS_WITH
--- INSERT INTO works_with VALUES(105, 400, 55000);
--- INSERT INTO works_with VALUES(102, 401, 267000);
--- INSERT INTO works_with VALUES(108, 402, 22500);
--- INSERT INTO works_with VALUES(107, 403, 5000);
--- INSERT INTO works_with VALUES(108, 403, 12000);
--- INSERT INTO works_with VALUES(105, 404, 33000);
--- INSERT INTO works_with VALUES(107, 405, 26000);
--- INSERT INTO works_with VALUES(102, 406, 15000);
--- INSERT INTO works_with VALUES(105, 406, 130000);
+INSERT INTO works_with VALUES(105, 400, 55000);
+INSERT INTO works_with VALUES(102, 401, 267000);
+INSERT INTO works_with VALUES(108, 402, 22500);
+INSERT INTO works_with VALUES(107, 403, 5000);
+INSERT INTO works_with VALUES(108, 403, 12000);
+INSERT INTO works_with VALUES(105, 404, 33000);
+INSERT INTO works_with VALUES(107, 405, 26000);
+INSERT INTO works_with VALUES(102, 406, 15000);
+INSERT INTO works_with VALUES(105, 406, 130000);
 
 -- task 1 find all employees
 
