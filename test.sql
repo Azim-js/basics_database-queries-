4b75d5aa5712d4f910a3063e5a175edfe2bbe977
Azim <66790056+Azim-js@users.noreply.github.com>
2021-04-05 20:21:08 +0530

    -- union with selecting two columns (unique ids)

--- test.sql
+++ test.sql
@@ -247,3 +247,13 @@ SELECT * FROM client WHERE client_name LIKE '%school%';
 -- TAsk to find list of all employee and branch names 
 
 SELECT first_name  AS company_names FROM employee UNION SELECT branch_name FROM branch;
+
+-- find a list of all clients and `branch_supplier` 
+
+SELECT client_name,branch_id FROM client  UNION SELECT supplier_name,branch_id FROM branch_supplier;
+
+-- rule
+-- 1 same number of columns
+-- 2 similar Datatype 
+
+SELECT client_name,client.branch_id From client UNION SELECT supplier_name,branch_supplier.branch_id FROM branch_supplier; --recommended 
