ec6b67b064a20ef923e95b04c32cd4d8f474cb4d
Azim <66790056+Azim-js@users.noreply.github.com>
2021-04-05 22:27:59 +0530

    --  NESTED QUERIES
    (multiple SELECT STATEMENTS using IN keyword)

--- test.sql
+++ test.sql
@@ -300,3 +300,16 @@ SELECT employee.emp_id,employee.first_name,branch.branch_name
 FROM employee 
 RIGHT JOIN branch 
 ON employee.emp_id=branch.mgr_id;
+
+-- ------------------------------------------ NESTED QUERIES ------------------------------------------------
+
+-- multiple SELECT STATEMENTS ;
+-- using IN keyword
+
+-- TAsk to find names of all employees who have sold over 30,000 to a single client 
+
+SELECT employee.first_name AS forename,employee.last_name AS surname
+FROM employee 
+WHERE employee.emp_id IN(
+    SELECT works_with.emp_id FROM works_with WHERE works_with.total_sales>30000
+);
