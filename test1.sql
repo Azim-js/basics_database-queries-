3a86d298064ce3129db748b5306a52780876657a
Azim <66790056+Azim-js@users.noreply.github.com>
2021-04-06 10:44:59 +0530

    -- TRIGGERS

---
+++ test1.sql
@@ -0,0 +1,12 @@
+-- -------------------------------------------------------------------- TRIGGERS -------------------------------------------------------------------------
+
+-- TRGIGGER is a block of sql defined to do a certain action 
+-- TO do something if an entry is added or deleted
+
+-- creating a table to set trigger
+
+CREATE TABLE trigger_set(
+    message VARCHAR(100)
+);
+
+-- seting up a trigger for trigger_Set table
